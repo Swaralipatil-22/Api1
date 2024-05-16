18ff41d5c6c95655889ec71ecb28cc30f660087b
“Swaralipatil-22” <swaralipatil408@gmail.com>
2024-05-16 10:48:38 +0530

    new commit

---
+++ models/user.js
@@ -0,0 +1,21 @@
+// models/User.js
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+    });
+
+    return User;
+};
